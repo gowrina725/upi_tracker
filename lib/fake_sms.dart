949df47a67856f07c34ff93f524351c79d01a100
hennahk <henna_b24096cs@nitc.ac.in>
2026-05-02 00:33:29 +0530

    Test commit to link GitHub account

---
+++ lib/fake_sms.dart
@@ -0,0 +1,4 @@
+const fakeUpiMessages = [
+  "INR 250.00 debited from A/c XXXX via UPI Ref 123456. Paid to SWIGGY.",
+  "Rs 120 credited to your account via UPI Ref 777888. From Riya.",
+];
--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,17 @@ import 'screens/expense_entry_screen.dart';
 import 'screens/expense_list_screen.dart';
 
 void main() {
-  runApp(const UpITrackerApp());
-import 'package:permission_handler/permission_handler.dart';
-
-void main() async {
-  import 'package:permission_handler/permission_handler.dart';
-
-  void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await requestSmsPermission();
-    runApp(const MyApp());
-  }
-
-  Future<void> requestSmsPermission() async {
-    PermissionStatus status = await Permission.sms.request();
-
-    if (status.isGranted) {
-      print("SMS Permission Granted");
-    } else {
-      print("SMS Permission Denied");
-    }
-  }
+  runApp(const UpiTrackerApp());
 }
 
-class UpITrackerApp extends StatelessWidget {
-  const UpITrackerApp({super.key});
+class UpiTrackerApp extends StatelessWidget {
+  const UpiTrackerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'UPI Expense Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       routes: {
         ExpenseListScreen.routeName: (_) => const ExpenseListScreen(),
